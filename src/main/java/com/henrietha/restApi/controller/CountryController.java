a6d33b48caf8bd16030d0d25e3a2ac17aa9a8292
Henrietha <areathy33@gmail.com>
2021-06-20 15:52:18 +0100

    Edited api consumption

--- src/main/java/com/henrietha/restApi/controller/CountryController.java
+++ src/main/java/com/henrietha/restApi/controller/CountryController.java
@@ -3,9 +3,7 @@ package com.henrietha.restApi.controller;
 import com.henrietha.restApi.domain.response.GetCountryResponse;
 import com.henrietha.restApi.service.ConsumeRestAPI;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,22 +13,28 @@ import java.util.List;
 public class CountryController {
 
     @Autowired
-    private ConsumeRestAPI apiService;
+    private ConsumeRestAPI consumeRestAPI;
 
     @GetMapping(value = "/list")
     public List<?> listCountries() {
-        List<GetCountryResponse> response  = apiService.getBooks2();
+        List<GetCountryResponse> response  = consumeRestAPI.getBooks2();
         List<String> names = new ArrayList<>();
         for (GetCountryResponse res: response) {
             names.add(res.getTranslation().getBr());
         }
+
+//        (GetCountryResponse res: response -> (names.add(res.getTranslation().getBr()));
+
         return names;
     }
 
-    @GetMapping (value = "/extra")
+    @GetMapping(value = "/extra")
     public String extraCalls() {
-        List<GetCountryResponse> response  = apiService.getBooks2();
-        String names = new String();
-        return names;
+        String response  = consumeRestAPI.getBooks();
+//        String google = response.substring(0,30);
+
+        return response;
     }
+
+
 }
--- src/main/java/com/henrietha/restApi/service/ConsumeRestAPI.java
+++ src/main/java/com/henrietha/restApi/service/ConsumeRestAPI.java
@@ -3,9 +3,6 @@ package com.henrietha.restApi.service;
 import com.henrietha.restApi.domain.response.GetCountryResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -23,7 +20,6 @@ public class ConsumeRestAPI {
         return Arrays.asList(getCountryResponses);
     }
 
-    @GetMapping(value = "/extra")
     public String getBooks() {
         String url = "https://www.google.com";
         String objects = restTemplate.getForObject(url, String.class);
@@ -31,10 +27,18 @@ public class ConsumeRestAPI {
     }
 
 
+
 //    @GetMapping("/getJson")
 //    public List<Object> getBooks2() {
 //        String url = "https://restcountries.eu/rest/v2/all";
 //        Object[] objects = restTemplate.getForObject(url, Object[].class);
 //        return Arrays.asList(objects);
+//    }
+
+//    @GetMapping(value = "/extra")
+//    public String getBooks() {
+//        String url = "https://www.google.com";
+//        String objects = restTemplate.getForObject(url, String.class);
+//        return objects;
 //    }
 }
